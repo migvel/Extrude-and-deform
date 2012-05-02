b3341930d37804ebdb29dff3be9e4183eb5b7771
Your Name <miguel.jgz@gmail.com>
2012-05-02 21:35:36 +0200

    class added and bugfix

--- extrudeform.py
+++ extrudeform.py
@@ -3,6 +3,7 @@ from io_curve_svg import import_svg
 import re
 import cmath
 import math
+import sys
 
 # functions
 def find_object(targetname):
@@ -21,180 +22,175 @@ def log_vertices(filename,mesh):
         FILE.write(strline)
     FILE.close()
 
-def get_object_center(mesh):
-    """ Returns the object distance to center """
-    xmax = xmin = mesh.vertices[0].co[0]
-    ymax = ymin = mesh.vertices[0].co[1]
-
-    for index,vertex in enumerate(mesh.vertices):
-        if(vertex.co[0]>xmax):
-            xmax = vertex.co[0]
-        elif(vertex.co[0]<xmin):
-            xmin = vertex.co[0]
-        if(vertex.co[1]>ymax):
-            ymax = vertex.co[1]
-        elif(vertex.co[1]<ymin):
-            ymin = vertex.co[1]
-            
-    return(abs(xmax) - (abs(xmax) - abs(xmin)) / 2 , abs(ymax) - (abs(ymax) - abs(ymin)) / 2)
-
-def create_mesh(meshname,vertices,sides):
-    """ create mesh from vertices and sides """
-    newmesh = bpy.data.meshes.new(meshname)
-    newmesh.from_pydata(vertices, [], sides)
-    newmesh.update()
-    return(newmesh)
-    
-def create_object(objectname,mesh):
-    newobject = bpy.data.objects.new(objectname,mesh)
-    newobject.data = newmesh
-    return(newobject)
-
-def create_vertices(deep,levels):
-    """ Create vertices of extrusion """
-    vertices = []
-    for zlevel in range(0,deep,int(deep/levels)):
-        print(int(deep/levels))
-        for idx,vertex in enumerate(mesh.vertices):
-            vertices.append((vertex.co[0],vertex.co[1],zlevel))
-    return(vertices)
-
 def deselect_all_objects():
     """ Deselects all objects """
     for object in bpy.data.objects:
         object.select = False
 
-def create_face(vertexindex,nlayervertices):
-    """ Make an square face starting in index counter clockwise """
-    abovevertex = find_above_vertex(vertexindex,nlayervertices)
-    purevertex = abovevertex - (int(abovevertex / nlayervertices) * nlayervertices)
 
-    if(purevertex == nlayervertices-1):
-        aboveleftvertex = ( int(abovevertex / nlayervertices) * nlayervertices)
-    else:
-        aboveleftvertex = abovevertex+1
+#main class
+class ExtrudeForm:
+    #class for extruding and object and deform in the xy.
+    Name = "ExtrudeForm"
 
-    bellowleftvertex = find_bellow_vertex(aboveleftvertex,nlayervertices)
-    face = []
+    def __init__(self,curves):
+        self.obj = bpy.context.active_object
+        bpy.ops.object.convert(target='MESH',keep_original=False)
+        self.mesh = 0
+        
+    def extrude(self,deep,levels):
+        """ Extrudes the plane mesh """
+        self.mesh  = self.obj.data
+        
+        #center
+        self.get_object_coordinates()        
+        self.obj.location = (( -self.xcent , -self.ycent, 0.0))
+        bpy.ops.object.transform_apply(location=True, rotation=False, scale=False)
 
-    face.append(vertexindex)
-    face.append(abovevertex)
-    face.append(aboveleftvertex)
-    face.append(bellowleftvertex)
-    return(face)
+        self.nvertices = len(self.mesh.vertices)
+     
+        self.create_vertices(deep,levels)
+        self.create_faces(levels)
+        
+        self.create_mesh("newmesh")    
+        self.create_object("newobject")
+
+        scene = bpy.context.scene
+        scene.objects.link(self.newobject)
+        scene.objects.active = self.newobject
+        self.newobject.select = True
+
+    def deform(self,method,mathformula1,mathformula2):
+        """ deform the object with different formulas """
+        if(method == "orthogonal"):
+            self.modify_orthogonal_mesh(mathformula1,mathformula2)
+        elif(method == "polar"):
+            self.modify_polar_mesh(mathformula1,mathformula2)
+
+    def create_vertices(self,deep,levels):
+        """ Create vertices of extrusion """
+        self.vertices = []
+        for zlevel in range(0,deep,int(deep/levels)):
+            for idx,vertex in enumerate(self.mesh.vertices):
+                self.vertices.append((vertex.co[0],vertex.co[1],zlevel))
     
+    def create_faces(self,nlevels):
+        """ Create the faces for all the mesh """
+        self.faces = []
+        for levels in range(nlevels-1):
+            for vertex in range(self.nvertices):
+                self.create_face(vertex + levels * self.nvertices)
+                self.faces.append(self.face)
+    
+    def create_face(self,vertexindex):
+        """ Make an square face starting in index counter clockwise """
+        self.find_above_vertex(vertexindex)
+        purevertex = self.abovevertex - (int(self.abovevertex / self.nvertices) * self.nvertices)
 
-def create_faces(nlayervertices,nlevels):
-    """ Create the faces for all the mesh """
-    faces = []
-    for levels in range(nlevels-1):
-        for vertex in range(nlayervertices):
-            face = create_face(vertex + levels * nlayervertices,nlayervertices)
-            faces.append(face)
-    return(faces)
+        if(purevertex == self.nvertices-1):
+            aboveleftvertex = ( int(self.abovevertex / self.nvertices) * self.nvertices)
+        else:
+            aboveleftvertex = self.abovevertex+1
 
-def find_above_vertex(vertexindex,nlayervertices):
-    """ finds vertex above from a given one """
-    return(vertexindex+nlayervertices)
+        self.find_bellow_vertex(aboveleftvertex)
+        self.face = []
 
-def find_bellow_vertex(vertexindex,nlayervertices):
-    """ finds vertex bellow from given one """
-    return(vertexindex-nlayervertices)
+        self.face.append(vertexindex)
+        self.face.append(self.abovevertex)
+        self.face.append(aboveleftvertex)
+        self.face.append(self.bellowleftvertex)
 
-def modify_polar_mesh(mathstringphi,mathstringr,mesh,nlayervertices):
-    """ Modifies in a polar way a mesh """
+    def create_mesh(self,meshname):
+        """ create mesh from vertices and sides """
+        self.newmesh = bpy.data.meshes.new(meshname)
+        self.newmesh.from_pydata(self.vertices, [], self.faces)
+        self.newmesh.update()
 
-    for idx,vertex in enumerate(mesh.vertices):
-        r = p  = int(idx / nlayervertices)
+    def create_object(self,objectname):
+        self.newobject = bpy.data.objects.new(objectname,self.newmesh)
+        self.newobject.data = self.newmesh
+
+    def modify_polar_mesh(self,mathstringphi,mathstringr):
+        """ Modifies in a polar way a mesh """
+        for idx,vertex in enumerate(self.newmesh.vertices):
+            r = p  = int(idx / self.nvertices)
+            r,phi = cmath.polar(complex(vertex.co[0],vertex.co[1]))
+
+            if(mathstringr != ""):
+                self.math_parser(mathstringr)
+                modr = eval(self.formula)
+                r = r + modr
+
+            if(mathstringphi != ""):
+                self.math_parser(mathstringphi)
+                modphi = eval(self.formula)
+                phi = phi + modphi
         
-        r,phi = cmath.polar(complex(vertex.co[0],vertex.co[1]))
+            n = cmath.rect(r,phi)
+            vertex.co[0]= n.real
+            vertex.co[1] = n.imag       
 
-        if(mathstringr != ""):
-            formular = math_parser(mathstringr)
-            modr = eval(formular)
-            r = r + modr
-        if(mathstringphi != ""):
-            formulaphi = math_parser(mathstringphi)
-            modphi = eval(formulaphi)
-            phi = phi + modphi
+    def modify_orthogonal_mesh(self,mathstringx,mathstringy):
+        """ Modifies in a orthogonal way a mesh """ 
+        for idx,vertex in enumerate(self.newmesh.vertices):
+            
+            x = y = int(idx / self.nvertices)
+            if(mathstringx != ""):
+                self.math_parser(mathstringx)
+                modx = eval(self.formula)
+                vertex.co[0] = vertex.co[0] * modx
         
-        n = cmath.rect(r,phi)
-        vertex.co[0]= n.real
-        vertex.co[1] = n.imag       
+            if(mathstringy != ""):
+                self.math_parser(mathstringy)
+                mody = eval(self.formula)
+                vertex.co[1] = vertex.co[1] * mody
 
-    return(mesh)
-        
-def modify_orthogonal_mesh(mathstringx,mathstringy,mesh,nlayervertices):
-    """ Modifies in a orthogonal way a mesh """ 
-    for idx,vertex in enumerate(mesh.vertices):
+            
+    def get_object_coordinates(self):
+        """ Returns the object distance to center """
+        xmax = xmin = self.mesh.vertices[0].co[0]
+        ymax = ymin = self.mesh.vertices[0].co[1]
 
-        x = y = int(idx / nlayervertices)
-        
-        formulax = math_parser(mathstringx)
-        formulay = math_parser(mathstringy)
-        
-        if(mathstringx != ""):
-            modx = eval(formulax)
-            vertex.co[0] = vertex.co[0] * modx
-        
-        if(mathstringy != ""):
-            mody = eval(formulay)
-            vertex.co[1] = vertex.co[1] * mody
+        for index,vertex in enumerate(self.mesh.vertices):
+            if(vertex.co[0]>xmax):
+                xmax = vertex.co[0]
+            elif(vertex.co[0]<xmin):
+                xmin = vertex.co[0]
+            if(vertex.co[1]>ymax):
+                ymax = vertex.co[1]
+            elif(vertex.co[1]<ymin):
+                ymin = vertex.co[1]
+            
+        self.xcent = xmax - (xmax - xmin ) / 2
+        self.ycent = ymax - (ymax - ymin ) / 2
 
-    return(mesh)
+    def find_above_vertex(self,vertexindex):
+        """ finds vertex above from a given one """
+        self.abovevertex = vertexindex+self.nvertices
 
+    def find_bellow_vertex(self,vertexindex):
+        """ finds vertex bellow from given one """
+        self.bellowleftvertex =  vertexindex-self.nvertices
 
-def math_parser(string):
-    """ pases a math formula into math module sintax """
-    symbols=['sin','cos','exp','pi','log','sqrt']
+    def math_parser(self,string):
+        """ pases a math formula into math module sintax """
+        symbols=['sin','cos','exp','pi','log','sqrt']
+        self.formula=string
+        for isymbol in symbols:
+            p = re.compile(isymbol)
+            for i in p.finditer(self.formula):
+               self.formula = self.formula[:i.start()]+'math.'+self.formula[i.start():]
+               
 
-    for isymbol in symbols:
-        p = re.compile(isymbol)
-        for i in p.finditer(string):
-            string=string[:i.start()]+'math.'+string[i.start():]
-    return(string)
+                                        
+                
+a = ExtrudeForm("hola")
+a.extrude(10,10)
+#a.deform("polar","p*0.1","r*0.4")
+a.deform("orthogonal","sqrt(0.1*x)+1","sqrt(0.1*y)+1")
 
-
-# main
-print("Importing svg file")
-import_svg.load_svg("path to svg file to import")
-
-idx = find_object("Curve")
-
-object = bpy.data.objects[idx[0]]
-object.select=1
-bpy.context.scene.objects.active = object
-bpy.ops.object.convert(target='MESH',keep_original=False)
-
-mesh = object.data
-nvertices = len(mesh.vertices)
-
-log_vertices("/home/sio2/logvertices.log",mesh)
-
-xcent,ycent = get_object_center(mesh)
-object.location=(-xcent, ycent, 0)
-bpy.ops.object.transform_apply(location=True, rotation=False, scale=False)
-
-vertices = create_vertices(10,10)
-faces = create_faces(nvertices,10)
-
-newmesh = create_mesh("newmesh",vertices,faces)    
-newobject =  create_object("newobject",newmesh)
-
-scene = bpy.context.scene
-scene.objects.link(newobject)
-scene.objects.active = newobject
-newobject.select = True
-
-deselect_all_objects()
-idx = find_object("Curve")
-object = bpy.data.objects[idx[0]]
-object.select=1
-bpy.ops.object.delete()
-
-#newmesh = modify_polar_mesh("p*0.1","r*0.4",newmesh,nvertices)
-newmesh = modify_polar_mesh("p*0.1","",newmesh,nvertices)
-#newmesh = modify_orthogonal_mesh("0.3*exp(x)+5","0.3*exp(y)+5",newmesh,nvertices)
-#newmesh = modify_orthogonal_mesh("sqrt(x)+1","",newmesh,nvertices)
-newmesh = modify_orthogonal_mesh("sqrt(x)+1","sqrt(y)+1",newmesh,nvertices)
-#newmesh = modify_orthogonal_mesh("abs(sin(x))+3","abs(sin(y))+3",newmesh,nvertices)
+# ("p*0.1","")
+# ("0.3*exp(x)+5","0.3*exp(y)+5")
+# ("sqrt(x)+1","")
+# ("sqrt(x)+1","sqrt(y)+1")
+# ("abs(sin(x))+3","abs(sin(y))+3")
